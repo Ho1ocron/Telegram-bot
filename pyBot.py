717cfcea38a1fc3fb974d2487159b00ddcc6dba1
Holocron <108415700+Ho1ocron@users.noreply.github.com>
2023-04-10 17:40:28 +0500

    Add files via upload

--- pyBot.py
+++ pyBot.py
@@ -2,9 +2,9 @@ import telebot
 import generator
 from telebot import types
 
-bot = telebot.TeleBot(token="Token")
+bot = telebot.TeleBot(token="5612661926:AAGi2cVBLFffgDvhBujxzkIZfWwUQJ2V6fo")
 
-ssubj = None
+ssubj = None #: This is name of the subj that user chose. While he don't make the choice it is None
 
 @bot.message_handler(content_types=["text"])
 def start(self):
@@ -12,16 +12,13 @@ def start(self):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=False)
     btn = [types.KeyboardButton for types.KeyboardButton in generator.sub.keys()]
     markup.add(*btn)
-    try:
-        if not self.text in commands.keys():
-            bot.send_message(self.chat.id, "Я вас не понимаю. Введите /start.", reply_markup=markup)
-            bot.register_next_step_handler(self, start)
-        else:
-            bot.send_message(self.chat.id, commands[self.text], reply_markup=markup)
-            bot.register_next_step_handler(self, subj_choice)
-            ssubj = self.text
-    except:
-        print("Error")
+    if not self.text in commands.keys():
+        bot.send_message(self.chat.id, "Я вас не понимаю. Введите /start.", reply_markup=markup)
+        bot.register_next_step_handler(self, start)
+    else:
+        bot.send_message(self.chat.id, commands[self.text], reply_markup=markup)
+        bot.register_next_step_handler(self, subj_choice)
+        ssubj = self.text
 
 
 def subj_choice(self):
@@ -53,28 +50,30 @@ def send_doc(self):
         bot.send_message(self.chat.id, "Я вас не понимаю. Введите название предмета или /start.", reply_markup=markup)
         bot.register_next_step_handler(self, start)
     else:
+
         try:
             doc = open(subj_ch[ssubj][self.text], "rb")
-            print(doc)
-            print(subj_ch[ssubj][self.text])
             bot.send_document(self.chat.id, doc, subj_ch[ssubj][self.text])
         except:
             print("He удалось отправить документ.")
+
         bot.send_message(self.chat.id, "Вот ваш документ.", reply_markup=markup)
         bot.register_next_step_handler(self, subj_choice)
+
 
 commands = {word: "Выбери класс персонажа." for word in generator.sub.keys()}
 commands["/start"] = "Привет, я помогу тебе c учебой. Напиши название предмета, и я скину тебе документацию. Ты можешь в любой момент ввести /start и перезапустить меня."
 
-fun = {word: send_doc for word in generator.sub.keys()}
-fun["/start"] = send_doc
 
-subj_ch = {word: {
-    "7 класс":f'{generator.sub["Русский"]}.docx', 
-    "8 класс":f'{generator.sub["Алгебра"]}.docx', 
-    "9 класс": f'{generator.sub["Информатика"]}.docx'
-    } for word in generator.sub.keys()}
+subj_ch = {
+    word: {
+        "7 класс":f'{generator.sub[word]}7.docx', 
+        "8 класс":f'{generator.sub[word]}8.docx', 
+        "9 класс": f'{generator.sub[word]}9.docx'
+        } for word in generator.sub.keys()
+    }
 
 docs = {word: f"{generator.sub[word]}.docx" for word in generator.sub.keys()}
 
 bot.polling(none_stop=True)
+
