9e57449b1e7bbe214983baeccb3262b6fcbedd12
Holocron <108415700+Ho1ocron@users.noreply.github.com>
2023-04-17 17:38:09 +0500

    Add files via upload

---
+++ pyBot1.py
@@ -0,0 +1,97 @@
+import telebot
+import generator
+import time
+from telebot import types
+text = []
+text1 = []
+bot = telebot.TeleBot(token="5612661926:AAGi2cVBLFffgDvhBujxzkIZfWwUQJ2V6fo")
+
+ssubj = None #: This is the name of the subj that user chose. While he don't make the choice it is None
+
+@bot.message_handler(content_types=["text"])
+def start(self):
+    global ssubj
+    global text
+    text.append(self.text)
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=False)
+    btn = [types.KeyboardButton for types.KeyboardButton in generator.sub.keys()]
+    markup.add(*btn)
+    if len(text) == 1 or len(text1) == 0:
+        if not self.text in commands.keys():
+            bot.send_message(self.chat.id, "Я вас не понимаю. Введите /start.", reply_markup=markup)
+            bot.register_next_step_handler(self, start)
+        else:
+            bot.send_message(self.chat.id, commands[self.text], reply_markup=markup)
+            bot.register_next_step_handler(self, subj_choice)
+            ssubj = self.text
+    else:
+        text.clear()
+        print(text)
+        bot.register_next_step_handler(self, start)
+
+
+
+def subj_choice(self):
+    global ssubj
+    global text1
+    text1.append(self.text)
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    btn = [types.KeyboardButton for types.KeyboardButton in generator.sub.keys()]
+    markup.add(*btn)
+    if len(text1) == 1 or len(text1) == 0:
+        if not self.text in commands.keys():
+            bot.send_message(self.chat.id, "Я вас не понимаю. Введите название предмета или /start.", reply_markup=markup)
+            bot.register_next_step_handler(self, start)
+        else:
+            ssubj = self.text
+            markup1 = types.ReplyKeyboardMarkup(resize_keyboard=True)
+            if self.text != "/start":
+                btn1 = [types.KeyboardButton for types.KeyboardButton in subj_ch[self.text]]
+                markup1.add(*btn1)
+                bot.send_message(self.chat.id, commands[self.text], reply_markup=markup1)
+                bot.register_next_step_handler(self, send_doc)
+            else:
+                bot.send_message(self.chat.id, commands[self.text], reply_markup=markup)
+                bot.register_next_step_handler(self, start)
+    else:
+        text1.clear()
+        print(text1)
+        bot.register_next_step_handler(self, start)
+
+def send_doc(self):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    btn = [types.KeyboardButton for types.KeyboardButton in generator.sub.keys()]
+    markup.add(*btn)
+    if not self.text in subj_ch[ssubj].keys():
+        bot.send_message(self.chat.id, "Я вас не понимаю. Введите название предмета или /start.", reply_markup=markup)
+        bot.register_next_step_handler(self, start)
+    else:
+
+        try:
+            doc = open(subj_ch[ssubj][self.text], "rb")
+            bot.send_document(self.chat.id, doc, subj_ch[ssubj][self.text])
+            bot.send_message(self.chat.id, "Вот ваш документ. /start", reply_markup=markup)
+            bot.register_next_step_handler(self, start)
+            
+        except:
+            print("He удалось отправить документ.")
+            bot.send_message(self.chat.id, "He удалось отправить документ.", reply_markup=markup)
+            bot.register_next_step_handler(self, start)
+
+
+commands = {word: "Выбери класс." for word in generator.sub.keys()}
+commands["/start"] = "Привет, я помогу тебе c учебой. Напиши название предмета, и я скину тебе документацию. Ты можешь в любой момент ввести /start и перезапустить меня."
+
+
+subj_ch = {
+    word: {
+        "7 класс":f'{generator.sub[word]}7.docx', 
+        "8 класс":f'{generator.sub[word]}8.docx', 
+        "9 класс": f'{generator.sub[word]}9.docx'
+        } for word in generator.sub.keys()
+    }
+
+docs = {word: f"{generator.sub[word]}.docx" for word in generator.sub.keys()}
+
+bot.polling(none_stop=True)
+
